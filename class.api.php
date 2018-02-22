dbef3db66807c9bfecfc753c672bc4f896ad9fcf
Maksim Gorbunov <gorbunovnn@gmail.com>
2018-02-22 20:25:29 +0300

    Returnable error object is updated in signedRequest()

--- class.api.php
+++ class.api.php
@@ -116,7 +116,7 @@ class phlowAPI {
 		$result = json_decode($response);
 		curl_close($curl);
 
-		return ($status == 200) ? $result : array('error' => $response);
+		return ($status == 200) ? $result : array('error' => $result);
 	}
 
 	private function getPageURL($isAjaxCall = false){
